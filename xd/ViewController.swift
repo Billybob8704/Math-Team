a1aa309616a99f66a609f30b288199ec75eb17f4
Billybob8704 <58694971+Billybob8704@users.noreply.github.com>
2020-01-06 08:12:30 -0600

    Testing
    I played around with stuff

--- xd/ViewController.swift
+++ xd/ViewController.swift
@@ -15,6 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("This is a test another test")
     }
-
+    @IBDesignable
+    class GradientView: UIView {
+        @IBInspectable var firstColor: UIColor = UIColor.clear {
+            didSet {
+                updateView()
+            }
+        }
+        @IBInspectable var secondColor: UIColor = UIColor.clear {
+            didSet {
+                updateView()
+            }
+        }
+        
+        func updateView() {
+        }
+        override class var layerClass: AnyClass {
+            get {
+                return CAGradientLayer.self
+            }
+        }
+        func uspdateView() {
+            let layer = self.layer as! CAGradientLayer
+            layer.colors = [firstColor, secondColor].map{$0.cgColor}
+        }
+    }
+    
 }
 
