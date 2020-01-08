c0d2f425efeb012b717e4aa4eeca3479be8a28cb
Billybob8704 <58694971+Billybob8704@users.noreply.github.com>
2020-01-08 08:11:21 -0600

    Begun the navigation menu

---
+++ xd/Utils/Protocols.swift
@@ -0,0 +1,11 @@
+//
+//  Protocols.swift
+//  xd
+//
+//  Created by FOREMAN, CHRISTIAN on 1/8/20.
+//  Copyright © 2020 district196.org. All rights reserved.
+//
+
+protocol HomeControllerDelegate {
+    func handleMenuToggle(forMenuOption menuOption: MenuOption?)
+}
