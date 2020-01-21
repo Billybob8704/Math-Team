3245c3130315c4d5cc969afe2fb7bd4997f05840
Billybob8704 <58694971+Billybob8704@users.noreply.github.com>
2020-01-21 07:54:54 -0600

    Added Navigation to Practice Page

--- xd/Controller/PracticeController.swift
+++ xd/Controller/PracticeController.swift
@@ -11,20 +11,32 @@ import UIKit
 class PracticeController: UIViewController {
     
     //Mark: - Properties
+    var delegate: HomeControllerDelegate?
     
     //Mark: - Init
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        view.setGradientBackground(colorOne: Colors.awwwwwwwww, colorTwo: Colors.blue)
+        configureNavigationBar()
         
     }
     //MARK: - Helper Functions
     
     func ConfigueUI() {
         view.setGradientBackground(colorOne: Colors.awwwwwwwww, colorTwo: Colors.blue)
-        
     
+    }
+    
+    @objc func handleMenuToggle() {
+        delegate?.handleMenuToggle(forMenuOption: nil)
+    }
+    
+    func configureNavigationBar() {
+        navigationController?.navigationBar.barTintColor = .darkGray
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationItem.title = "The Epic Math Team App"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_menu_white_3x").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleMenuToggle))
     }
 }
